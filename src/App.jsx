0f611ae6c62e8796af830113963c17ebe7264929
Ajandro <adelaygleisafleitas@gmail.com>
2024-02-18 15:02:58 -0500

    git init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import Title from "./componets/Title";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [contador, setContador] = useState(0);
+  const handleClipIncrementar = () => {
+    setContador((contador) => (contador += 1));
+  };
+  const isImpar = () => {
+    if (contador % 2 == 0) {
+      return true;
+    } else {
+      false;
+    }
+  };
+  const handleClipReset = () => {
+    setContador(0);
+  };
+
+  const handleClipRestar = () => {
+    setContador((contador) => (contador -= 1));
+  };
+
   return (
     <>
+      <header>
+        <h1>Clase_Midu_Dev_hook</h1>
+      </header>
       <main className="container">
-        <Title color="blue" titulo="Hello" />
-        <Title color="red" titulo="Word" />
-        <Title color="black" titulo="Alex" />
+        D
+        <section>
+          <p>{contador}</p>
+          <p>{isImpar() ? "Es par" : "Es impar"}</p>
+          <strong>{contador == 0 ? "comienzo" : ""}</strong>
+          <br />
+          <strong>
+            {contador === 0 ? "" : contador > 0 ? "Positivo" : "negativo"}
+          </strong>
+          <br />
+          <button onClick={handleClipIncrementar}>Incrementar</button>
+          <button onClick={handleClipReset}>Reset</button>
+          <button onClick={handleClipRestar}>Restar</button>
+        </section>
       </main>
     </>
   );
